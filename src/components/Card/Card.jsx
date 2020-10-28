ee7dee62c0f8f7d9ff3797573ba8398eccba07c8
Binod Shrestha <shresthabinod.softdev@gmail.com>
2020-10-28 17:03:01 -0400

    first commit

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import {CardContent, Typography, Grid, Card} from "@material-ui/core";
+import {styles} from './card.module.css'
+
+const Cards = () => {
+    return(
+        <div >
+            <Grid container spacing={2} justify="center">
+                <Grid item component={Card}>
+               <CardContent>
+                   <Typography color="textSecondary" gutterBottom>Project</Typography>
+                   <Typography variant="h6" gutterBottom>Description</Typography>
+                   <Typography variant="body2" gutterBottom>Description:</Typography>
+               </CardContent>
+            </Grid>
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>Skills</Typography>
+                        <Typography variant="h6" gutterBottom>Description</Typography>
+                        <Typography variant="body2" gutterBottom>Description:</Typography>
+                    </CardContent>
+                </Grid>
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>Education</Typography>
+                        <Typography variant="h6" gutterBottom>Description</Typography>
+                        <Typography variant="body2" gutterBottom>Description:</Typography>
+                    </CardContent>
+                </Grid>
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>Hackathon</Typography>
+                        <Typography variant="h6" gutterBottom>Description</Typography>
+                        <Typography variant="body2" gutterBottom>Description:</Typography>
+                    </CardContent>
+                </Grid>
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>Resume</Typography>
+                        <Typography variant="h6" gutterBottom>Description</Typography>
+                        <Typography variant="body2" gutterBottom>Description:</Typography>
+                    </CardContent>
+                </Grid>
+            </Grid>
+
+        </div>
+    );
+}
+export default Cards
---
+++ src/components/Navbar/MenuItem.js
@@ -0,0 +1,37 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url:"#",
+        cName:'nav-links'
+    },
+    {
+        title: 'Skills',
+        url:"#",
+        cName:'nav-links'
+    },
+    {
+        title: 'Projects',
+        url:"#",
+        cName:'nav-links'
+    },
+    {
+        title: 'Education',
+        url:"#",
+        cName:'nav-links'
+    },
+    {
+        title: 'Resume',
+        url:"#",
+        cName:'nav-links'
+    },
+    {
+        title: 'Volunteer',
+        url:"#",
+        cName:'nav-links'
+    },
+    {
+        title: 'Contact me',
+        url:"#",
+        cName:'nav-links'
+    }
+]
